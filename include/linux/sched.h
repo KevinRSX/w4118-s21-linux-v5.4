2176f0b099979022e7fe971e5d0265b369e4fd01
kevinrsx <kevinlgc@foxmail.com>
2021-03-04 23:35:21 -0800

    Put wrr_info to sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -266,6 +266,17 @@ struct vtime {
 };
 
 /*
+ * For wrr
+ */
+#define MAX_CPUS 8
+
+struct wrr_info {
+	int num_cpus;
+	int nr_running[MAX_CPUS];
+	int total_weight[MAX_CPUS];
+};
+
+/*
  * Utilization clamp constraints.
  * @UCLAMP_MIN:	Minimum utilization
  * @UCLAMP_MAX:	Maximum utilization
