f427fe6fcb976474ab66d7da5e76a04febea640e
Andreas <howangandreas@gmail.com>
2021-03-14 01:11:26 -0800

    update test.c: better and working wrappers

--- test.c
+++ test.c
@@ -45,32 +45,79 @@ void print_wrr_info(struct wrr_info buf)
 	printf("\n");
 }
 
-int main(void)
+void get_and_print_wrr_info()
 {
-	printf("# of CPUs: %d/%d\n", get_nprocs(), get_nprocs_conf());
-
-	struct wrr_info buf = { MAX_CPUS };
-
-	//Garbage values
-	buf.nr_running[3] = 133337;
-	buf.total_weight[7] = 133337;
-
 	int ret;
-
-	// TODO: Set some processes to use SCHED_WRR
-	// Useful function: sched_setscheduler
+	struct wrr_info buf = { MAX_CPUS };
 
 	printf("get_wrr_info:\n");
 	ret = syscall(__NR_SYSCALL_GET_WRR_INFO, &buf);
-	if (ret)
+	if (!ret)
 		fprintf(stderr, "err: %s\n", strerror(errno));
 
 	print_wrr_info(buf);
+}
 
-	printf("set_wrr_weight: %d\n", 20);
-	ret = syscall(__NR_SYSCALL_SET_WRR_WEIGHT, 20);
+/* wrapper of the syscall*/
+void set_wrr_weight(int weight)
+{
+	int ret;
+	printf("set_wrr_weight: %d\n", weight);
+	ret = syscall(__NR_SYSCALL_SET_WRR_WEIGHT, weight);
 	if (ret)
 		fprintf(stderr, "err: %s\n", strerror(errno));
+}
+
+int born_idle_child()
+{
+	pid_t pid;
+	pid = fork();
+	if (!pid) {
+		while (1)
+			;
+	} else if (pid < 0) {
+		fprintf(stderr, "fork err: %s", strerror(errno));
+		exit(-1);
+	}
+	// printf("> idle child is borned and running.\n");
+	return pid;
+}
+
+/*wrapper of the sched_setscheduler*/
+void setscheduler(pid_t pid, int policy)
+{
+	/* The param is for Non-RT policies */
+	struct sched_param param = { 0 };
+
+	int ret;
+	ret = sched_setscheduler(pid, policy, &param);
+	if (ret < 0) {
+		fprintf(stderr, "err: %s\n", strerror(errno));
+		exit(ret);
+	}
+}
+
+int main(void)
+{
+	// printf("# of CPUs: %d/%d\n", get_nprocs(), get_nprocs_conf());
+	get_and_print_wrr_info();
+
+	//Set some processes to use SCHED_WRR
+	int children_num = 20;
+	int children_pids[children_num];
+	for (int i = 0; i < children_num; i++) {
+		children_pids[i] = born_idle_child();
+		setscheduler(children_pids[i], SCHED_WRR);
+	}
+
+	// get_and_print_wrr_info();
+
+	for (int i = 0; i < children_num; i++) {
+		kill(children_pids[i], SIGKILL);
+		get_and_print_wrr_info();
+	}
+
+	// get_and_print_wrr_info();
 
 	return 0;
 }
